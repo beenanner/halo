7711371b45f130060b61656219280bc7edc879fd
Jonathan Lee <jonjohn1232009@gmail.com>
2016-06-07 22:58:54 -0400

    WIP: attempt to get jsdom working with perfCascade

--- routes/view.js
+++ routes/view.js
@@ -15,16 +15,25 @@ router.get('/', function (req, res, next) {
         if (!err){
           var data = JSON.parse(data);
           // Print all of the news items on Hacker News
-          console.log(perfCasc);
-          jsdom.env({
-            html: '<script>var data=' + data + ';</script><div id="output"></div>',
-            scripts: [perfCasc],
-            done: function (errors, window) {
-              var test = window.document.getElementById('output');
-                result = test.outerHTML;
-              res.render('view', { title: 'HAR Waterfall', harData: result });
-            }
-          });
+          var jsdom = require("node-jsdom").jsdom;
+          var window = jsdom().parentWindow;
+
+          window.__myObject = { foo: "bar" };
+          var divEl = window.document.createElement("div")
+          divEl.id = "output";
+          window.document.body.appendChild(divEl);
+          var dataScriptEl = window.document.createElement("script");
+          dataScriptEl.innerHTML = "var harData = " + JSON.stringify(data) + ";"
+          window.document.body.appendChild(dataScriptEl);
+          var scriptEl = window.document.createElement("script");
+          scriptEl.innerHTML = perfCasc;
+          window.document.body.appendChild(scriptEl);
+          var myScript = window.document.createElement("script");
+          myScript.innerHTML = "var options = {rowHeight: 23, showAlignmentHelpers : true, showIndicatorIcons: true, leftColumnWith: 25};var perfCascadeSvg =  perfCascade.fromHar(harData.log, options)";
+          window.document.body.appendChild(myScript);
+          var test = window.document.getElementById('output');
+          result = window.document.body.innerHTML;
+          res.render('view', { title: 'HAR Waterfall', harData: result });
         }else{
             console.log(err);
         }
