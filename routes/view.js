f80e5ca67f23a0e37dd4c925b23781e51cb40947
Jonathan Lee <jonjohn1232009@gmail.com>
2016-06-07 22:14:28 -0400

    save-point

--- routes/view.js
+++ routes/view.js
@@ -3,15 +3,28 @@ var fs      = require('fs');
 var nconf   = require('nconf');
 var path    = require('path');
 var router = express.Router();
+var jsdom = require('node-jsdom');
+var perfCasc = fs.readFileSync("node_modules/perf-cascade/src/dist/perf-cascade.js", "utf-8");
+
 
 router.get('/', function (req, res, next) {
   //fs.readFile('/public/uploads/'+req.query.har, 'utf8', function (err,data) {
     var filePath = path.join(process.cwd(), nconf.get('dir') + req.query.har);
-
+    var result;
     fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data){
         if (!err){
           var data = JSON.parse(data);
-          res.render('view', { title: 'HAR Waterfall', harData: JSON.stringify(data.log) });
+          // Print all of the news items on Hacker News
+          console.log(perfCasc);
+          jsdom.env({
+            html: '<script>var data=' + data + ';</script><div id="output"></div>',
+            scripts: [perfCasc],
+            done: function (errors, window) {
+              var test = window.document.getElementById('output');
+                result = test.outerHTML;
+              res.render('view', { title: 'HAR Waterfall', harData: result });
+            }
+          });
         }else{
             console.log(err);
         }
